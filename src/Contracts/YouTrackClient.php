5782c0b3b4e850d53b2245cdf7ad8d666b38129a
a-komarev <ell@cybercog.su>
2017-05-13 21:22:55 +0300

    Add null authenticator

--- src/Contracts/YouTrackClient.php
+++ src/Contracts/YouTrackClient.php
@@ -32,12 +32,19 @@ interface YouTrackClient
     public function setAuthenticator(RestAuthenticatorContract $authenticator): void;
 
     /**
-     * Create client authenticator instance.
+     * Get authentication strategy.
      *
-     * @param array $options
      * @return \Cog\YouTrack\Contracts\RestAuthenticator
      */
-    public function createAuthenticator(array $options): RestAuthenticatorContract;
+    public function getAuthenticator(): RestAuthenticatorContract;
+
+    /**
+     * Create client authenticator instance.
+     *
+     * @param string $authenticator
+     * @return \Cog\YouTrack\Contracts\RestAuthenticator
+     */
+    public function createAuthenticator(string $authenticator): RestAuthenticatorContract;
 
     /**
      * Create and send an HTTP request.
