930329bd12ed4169e18dc2cfc387eaff46481f7f
a-komarev <ell@cybercog.su>
2021-01-01 20:10:08 +0300

    Try to fix phpstan error

--- src/HttpClient/GuzzleHttpClient.php
+++ src/HttpClient/GuzzleHttpClient.php
@@ -88,7 +88,10 @@ class GuzzleHttpClient implements
     {
         $defaultAgent = 'GuzzleHttp/' . Client::VERSION;
         if (extension_loaded('curl') && function_exists('curl_version')) {
-            $defaultAgent .= ' curl/' . \curl_version()['version'];
+            $curlVersion = \curl_version();
+            if ($curlVersion !== false) {
+                $defaultAgent .= ' curl/' . \curl_version()['version'];
+            }
         }
         $defaultAgent .= ' PHP/' . PHP_VERSION;
 
